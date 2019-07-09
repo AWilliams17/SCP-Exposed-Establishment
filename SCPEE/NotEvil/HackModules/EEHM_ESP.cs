67d0143d86cdd3083b7148090192e4bd9cbd8402
Austin Williams <awilliams17412@gmail.com>
2019-07-09 06:17:21 -0500

    Location scanning added. Refactoring all of this next.

--- SCPEE/NotEvil/HackModules/EEHM_ESP.cs
+++ SCPEE/NotEvil/HackModules/EEHM_ESP.cs
@@ -48,6 +48,7 @@ namespace SCPEE.NotEvil.HackModules
                     scannedObjects.Clear();
                     ScanItems();
                     ScanPlayers();
+                    ScanLocations();
                 }
             }
         }
@@ -79,9 +80,9 @@ namespace SCPEE.NotEvil.HackModules
                 string itemLabel = playerInventory.availableItems[itemPickup.info.itemId].label;
                 Vector3 itemPosition = mainCamera.WorldToScreenPoint(itemPickup.transform.position);
                 int itemDistanceFromPlayer = (int)Vector3.Distance(mainCamera.transform.position, itemPickup.transform.position);
-                bool itemIsCloseEnough = itemDistanceFromPlayer <= 125;
+                bool itemIsCloseEnough = itemDistanceFromPlayer <= 100;
 
-                if (itemIsCloseEnough)
+                if (itemIsCloseEnough && itemPosition.z > 0f)
                 {
                     for (int i = 0; i < itemsOfInterest.Length; i++)
                     {
@@ -107,14 +108,59 @@ namespace SCPEE.NotEvil.HackModules
                     NicknameSync playerNicknameSync = player.transform.GetComponent<NicknameSync>();
                     CharacterClassManager playerClassManager = playerNicknameSync.GetComponent<CharacterClassManager>();
                     Vector3 playerPosition = mainCamera.WorldToScreenPoint(player.transform.position);
+
                     string playerClassname = playerClassManager.klasy[playerClassManager.curClass].fullName;
                     int playerDistanceFromLocalPlayer = (int)Vector3.Distance(mainCamera.transform.position, playerPosition);
-                    bool playerIsCloseEnough = playerDistanceFromLocalPlayer <= 125;
-                    if (playerIsCloseEnough)
+                    bool playerIsCloseEnough = playerDistanceFromLocalPlayer <= 100;
+
+                    if (playerIsCloseEnough && playerPosition.z > 0f)
                     {
                         ScannedObject scannedPlayer = new ScannedObject($"{playerClassname}:{playerDistanceFromLocalPlayer}m", playerPosition, Color.green);
                         scannedObjects.Add(scannedPlayer);
                     }
+                }
+            }
+        }
+        
+        private void ScanLocations()
+        {
+            Camera mainCamera = Camera.main;
+
+            // SCP 914
+            GameObject scp914 = GameObject.FindGameObjectWithTag("914_use");
+            Vector3 scp914Position = mainCamera.WorldToScreenPoint(scp914.transform.position);
+            if (scp914Position.z > 0f)
+            {
+                ScannedObject scanned914Location = new ScannedObject($"SCP 914", scp914Position, Color.yellow);
+                scannedObjects.Add(scanned914Location);
+            }
+
+            // Elevators
+            Lift[] lifts = FindObjectsOfType<Lift>();
+            for (int i = 0; i < lifts.Length; i++)
+            {
+                foreach (Lift.Elevator elevator in lifts[i].elevators)
+                {
+                    Vector3 elevatorPosition = mainCamera.WorldToScreenPoint(elevator.door.transform.position);
+                    if (elevatorPosition.z > 0f)
+                    {
+                        ScannedObject scannedElevator = new ScannedObject($"Elevator", elevatorPosition, Color.blue);
+                        scannedObjects.Add(scannedElevator);
+                    }
+                }
+            }
+
+            // Pocket Dimension Exits
+            foreach (PocketDimensionTeleport pdteleport in FindObjectsOfType<PocketDimensionTeleport>())
+            {
+                Vector3 teleportLocation = mainCamera.WorldToScreenPoint(pdteleport.transform.position);
+                int playerDistanceFromTeleporter = (int)Vector3.Distance(mainCamera.transform.position, teleportLocation);
+                bool teleporterIsCloseEnough = playerDistanceFromTeleporter <= 50;
+
+                if (teleporterIsCloseEnough && teleportLocation.z > 0f)
+                {
+                    ScannedObject scannedTeleporter = new ScannedObject($"Elevator:{playerDistanceFromTeleporter}m", teleportLocation, Color.white);
+                    scannedObjects.Add(scannedTeleporter);
                 }
             }
         }
