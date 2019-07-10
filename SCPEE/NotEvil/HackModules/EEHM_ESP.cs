88873480dc607d0ef770fb355069df0732d4dcb5
Austin Williams <awilliams17412@gmail.com>
2019-07-10 08:07:30 -0500

    Refactored. Re-integrating player scanning, and separating back into different scan functions.

--- SCPEE/NotEvil/HackModules/EEHM_ESP.cs
+++ SCPEE/NotEvil/HackModules/EEHM_ESP.cs
@@ -5,42 +5,19 @@ using System.Collections;
 
 namespace SCPEE.NotEvil.HackModules
 {
-    //internal class ScannedObject
-    //{
-    //    public string ScannedObjectLabel { get; private set; }
-    //    public Rect ScannedObjectPosition { get; private set; }
-    //    public Color ScannedObjectLabelColor { get; private set; }
-    //
-    //    public ScannedObject(string ObjectLabel, Vector3 ObjectScreenPoint, Color LabelColor)
-    //    {
-    //        ScannedObjectLabel = ObjectLabel;
-    //        ScannedObjectPosition = CalculatePositionRect(ObjectScreenPoint);
-    //        ScannedObjectLabelColor = LabelColor;
-    //    }
-    //
-    //    private static Rect CalculatePositionRect(Vector3 ObjectScreenPoint)
-    //    {
-    //        Rect positionRect = new Rect
-    //            (
-    //                ObjectScreenPoint.x - 20f, Screen.height - ObjectScreenPoint.y - 20f,
-    //                ObjectScreenPoint.x + 40f, Screen.height - ObjectScreenPoint.y + 50f
-    //            );
-    //        return positionRect;
-    //    }
-    //}
-    internal class ScannedObject
+    internal class ESPObject
     {
-        public string ScannedObjectLabel { get; private set; }
-        public GameObject ScannedGameObject { get; private set; }
-        public Color ScannedObjectLabelColor { get; private set; }
-        public int MinimumDistance { get; private set; }
+        public string ESPLabel { get; private set; }
+        public GameObject ESPGameObject { get; private set; }
+        public Color ESPLabelColor { get; private set; }
+        public int ESPMinimumDistance { get; private set; }
 
-        public ScannedObject(string ObjectLabel, GameObject @GameObject, Color LabelColor, int ObjectMinimumDistance)
+        public ESPObject(string Label, Color LabelColor, GameObject GameObject, int MinimumDistance)
         {
-            ScannedObjectLabel = ObjectLabel;
-            ScannedGameObject = @GameObject;
-            ScannedObjectLabelColor = LabelColor;
-            MinimumDistance = ObjectMinimumDistance;
+            ESPLabel = Label;
+            ESPLabelColor = LabelColor;
+            ESPGameObject = GameObject;
+            ESPMinimumDistance = MinimumDistance;
         }
     }
 
@@ -48,21 +25,7 @@ namespace SCPEE.NotEvil.HackModules
     {
         private bool isEnabled = false;
         private GameObject localPlayer = null;
-        private List<ScannedObject> scannedObjects = new List<ScannedObject>();
-
-        //private void AddScannedObjectIfViable(Transform ScannedObjectTransform, string GUILabel, Color GUILabelColor)
-        //{
-        //    Camera mainCamera = Camera.main;
-        //    Vector3 mainCameraPosition = mainCamera.transform.position;
-        //    Vector3 scannedObjectPosition = ScannedObjectTransform.position;
-        //    Vector3 scannedObjectScreenPoint = mainCamera.WorldToScreenPoint(scannedObjectPosition);
-        //    
-        //    if (!(Mathf.Abs(mainCameraPosition[2] - scannedObjectPosition[2]) > 300f) && scannedObjectScreenPoint.z > 0f)
-        //    {
-        //        ScannedObject scannedItem = new ScannedObject(GUILabel, scannedObjectScreenPoint, GUILabelColor);
-        //        scannedObjects.Add(scannedItem);
-        //    }
-        //}
+        private List<ESPObject> espObjects = new List<ESPObject>();
 
         private void Awake()
         {
@@ -74,9 +37,10 @@ namespace SCPEE.NotEvil.HackModules
             while (true)
             {
                 localPlayer = Utils.Misc.GetLocalPlayerGameObject();
-                scannedObjects.Clear();
                 if (isEnabled && localPlayer != null)
                 {
+                    espObjects.Clear();
+
                     string[] itemsOfInterest = {
                         "card", "p90", "com15", "rifle", "usp", "logicier",
                         "grenade", "pistol", "scorpion", "mp7", "epsilon",
@@ -93,17 +57,15 @@ namespace SCPEE.NotEvil.HackModules
                         {
                             if (itemLabel.ToLower().Contains(itemsOfInterest[i]))
                             {
-                                //AddScannedObjectIfViable(itemPickup.transform, itemLabel, Color.cyan);
-                                ScannedObject viableItemObject = new ScannedObject(itemLabel, itemPickup.gameObject, Color.cyan, 100);
-                                scannedObjects.Add(viableItemObject);
+                                ESPObject viableItemObject = new ESPObject(itemLabel, Color.cyan, itemPickup.gameObject, 100);
+                                espObjects.Add(viableItemObject);
                             }
                         }
                     }
 
                     // SCP 914
-                    ScannedObject scp914GameObject = new ScannedObject("SCP 914", GameObject.FindGameObjectWithTag("914_use"), Color.yellow, 150);
-                    scannedObjects.Add(scp914GameObject);
-                    //AddScannedObjectIfViable(scp914.transform, "SCP 914", Color.yellow);
+                    ESPObject scp914GameObject = new ESPObject("SCP 914", Color.yellow, GameObject.FindGameObjectWithTag("914_use"), 150);
+                    espObjects.Add(scp914GameObject);
 
                     // Elevators
                     Lift[] lifts = FindObjectsOfType<Lift>();
@@ -111,9 +73,8 @@ namespace SCPEE.NotEvil.HackModules
                     {
                         foreach (Lift.Elevator elevator in lifts[i].elevators)
                         {
-                            ScannedObject elevatorObject = new ScannedObject("Elevator", elevator.door.gameObject, Color.blue, 200);
-                            scannedObjects.Add(elevatorObject);
-                            //AddScannedObjectIfViable(elevator.door.transform, "Elevator", Color.blue);
+                            ESPObject elevatorObject = new ESPObject("Elevator", Color.blue, elevator.door.gameObject, 200);
+                            espObjects.Add(elevatorObject);
                         }
                     }
 
@@ -123,9 +84,8 @@ namespace SCPEE.NotEvil.HackModules
                         PocketDimensionTeleport.PDTeleportType teleporterType = pdTeleport.GetTeleportType();
                         if (teleporterType == PocketDimensionTeleport.PDTeleportType.Exit)
                         {
-                            ScannedObject exitTeleporterObject = new ScannedObject("Exit", pdTeleport.gameObject, Color.white, 75);
-                            scannedObjects.Add(exitTeleporterObject);
-                            //AddScannedObjectIfViable(pdTeleport.transform, "Teleporter Out", Color.white);
+                            ESPObject exitTeleporterObject = new ESPObject("Exit", Color.white, pdTeleport.gameObject, 75);
+                            espObjects.Add(exitTeleporterObject);
                         }
                     }
                 }
@@ -134,50 +94,36 @@ namespace SCPEE.NotEvil.HackModules
             }
         }
 
-
-
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Keypad2))
                 isEnabled = !isEnabled;
-
-            //if (isEnabled)
-            //{
-            //    localPlayer = Utils.Misc.GetLocalPlayerGameObject();
-            //    //if (localPlayer != null)
-            //    //{
-            //    //    ScanItems();
-            //    //    ScanPlayers();
-            //    //    ScanLocations();
-            //    //}
-            //}
         }
 
         private void OnGUI()
         {
-            foreach (ScannedObject scannedObject in scannedObjects)
+            foreach (ESPObject espObject in espObjects)
             {
                 Camera mainCamera = Camera.main;
                 Vector3 mainCameraPosition = mainCamera.transform.position;
-                Vector3 scannedObjectPosition = scannedObject.ScannedGameObject.transform.position;
-                Vector3 scannedObjectScreenPoint = mainCamera.WorldToScreenPoint(scannedObjectPosition);
-                int scannedObjectDistanceFromPlayer = (int)Vector3.Distance(mainCameraPosition, scannedObjectPosition);
+                Vector3 objectPosition = espObject.ESPGameObject.transform.position;
+                Vector3 objectScreenPoint = mainCamera.WorldToScreenPoint(objectPosition);
+                int objectDistanceFromPlayer = (int)Vector3.Distance(mainCameraPosition, objectPosition);
 
-                if (!(Mathf.Abs(mainCameraPosition[2] - scannedObjectPosition[2]) > 300f) && scannedObjectScreenPoint.z > 0f && scannedObjectDistanceFromPlayer <= scannedObject.MinimumDistance)
+                bool objectIsWithinRange = !(Mathf.Abs(mainCameraPosition[2] - objectPosition[2]) > 300f) && objectScreenPoint.z > 0f;
+                bool objectIsCloseEnough = objectDistanceFromPlayer <= espObject.ESPMinimumDistance;
+                if (objectIsWithinRange && objectIsCloseEnough)
                 {
                     Rect positionRect = new Rect
                         (
-                            scannedObjectScreenPoint.x - 20f, Screen.height - scannedObjectScreenPoint.y - 20f,
-                            scannedObjectScreenPoint.x + 40f, Screen.height - scannedObjectScreenPoint.y + 50f
+                            objectScreenPoint.x - 20f, Screen.height - objectScreenPoint.y - 20f,
+                            objectScreenPoint.x + 40f, Screen.height - objectScreenPoint.y + 50f
                         );
-                    GUI.color = scannedObject.ScannedObjectLabelColor;
-                    GUI.Label(positionRect, $"{scannedObject.ScannedObjectLabel} : {scannedObjectDistanceFromPlayer}");
+                    GUI.color = espObject.ESPLabelColor;
+                    GUI.Label(positionRect, $"{espObject.ESPLabel} : {objectDistanceFromPlayer}");
 
                 }
             }
         }
     }
 }
-
-
-
